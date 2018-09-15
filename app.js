4d32f606475458bb3215163d7454aa22fa4bfcc7
kitlingmui <kitlingmui@gmail.com>
2018-09-15 09:06:15 -0700

    Update V3

--- app.js
+++ app.js
@@ -14,15 +14,22 @@ var searchanimal = ""
 const apikey = "KCPUzUtfLCvsgh4tnabeT3hfxkgJwuB3"
 var queryURL = ""
 
-// Display current animals button
-for (let i=0; i< animals.length; i++){
-    $('#animal-btn').append(
-        `
-        <td><button type="button" class="btn btn-info animalbtn" data-value="${animals[i]}">${animals[i]}</button></td>
-        `
-    )
+// Function for displaying aniaml button
+function renderButtons() {
+    // Deletes the animal prior to adding new animal
+    $('#animal-btn').empty();
+    
+    // Display current animals button
+    for (let i=0; i< animals.length; i++){
+        $('#animal-btn').append(
+            `
+            <td><button type="button" class="btn btn-info animalbtn" data-value="${animals[i]}">${animals[i]}</button></td>
+            `
+        )
+    }   
 }
 
+renderButtons()
 
 // When on click one of the animal button
 $(document).on("click",".animalbtn", function(){
@@ -37,7 +44,7 @@ $(document).on("click",".animalbtn", function(){
         url: queryURL,
         method: "GET"
     }).then(function(r){
-        // console.log(r)
+        console.log(r)
         // console.log(r.data[0].rating);
         // console.log(r.data[0].embed_url);  
         // console.log(r.data.length)
@@ -47,11 +54,41 @@ $(document).on("click",".animalbtn", function(){
         for (let i=0; i<r.data.length; i++){
             
             $('.row-Result').append(`
-            <td>
+            <li>
                 <p>Rating: ${r.data[i].rating}</p>
-                <img id="animal-image" src="${r.data[i].images.preview_gif.url}" alt="animal">
-            </td>
+                <img id="animal-image" src="${r.data[i].images.original_still.url}" alt="animal">
+            </li>
             `)
         }  
     })   
 })
+
+
+// This function handles events where Add Aninmal "Submit" button is clicked
+$('#add-animal').on("click", function(event) {
+    // event.preventDefault() prevents the form from trying to submit itself.
+    // We're using a form so that the user can hit enter instead of clicking the button if they want
+    event.preventDefault();
+
+    // This line will grab the text from the input box
+    var newAnimal = $('#animal-input').val().trim();
+
+    console.log(newAnimal);
+    animals.push(newAnimal);
+
+    // The new animal from the textbox is then added to our array
+    // var isExist = false;
+
+    // for (let i=0; i< animals.length; i++){
+    //     if (animals[i] === newAnimal){
+    //         isExist = true;
+    //     }
+    // }
+    
+    // if (!isExist){
+        
+    // }
+
+    // calling renderButtons which handles the processing of our movie array
+    renderButtons();
+  });
